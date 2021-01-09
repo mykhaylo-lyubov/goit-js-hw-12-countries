febbcb5fb19da459fd323b57153fac8910418876
Mykhaylo Lyubov <mykhaylo.lyubov@gmail.com>
2021-01-09 09:58:56 +0200

    updates

--- src/js/pnotify.js
+++ src/js/pnotify.js
@@ -2,6 +2,8 @@ import { error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import { defaults } from '@pnotify/core';
-defaults.delay = '1000';
+defaults.delay = '2000';
+defaults.closer = false;
+defaults.sticker = false;
 
 export default error;
